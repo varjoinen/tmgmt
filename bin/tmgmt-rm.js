0c750f21e73dbaa6632929d9de74731b9ffad41a
Antti Varjoinen <antti@varjoinen.org>
2018-02-09 16:56:59 +0200

    Fix rm not working

--- bin/tmgmt-rm.js
+++ bin/tmgmt-rm.js
@@ -4,5 +4,5 @@ const report = require('../lib/report');
 
 program.parse(process.argv);
 
-report.remove(db, program.args[0])
+report.remove(database, program.args[0])
 .catch((e) => { console.log(e.message); });
